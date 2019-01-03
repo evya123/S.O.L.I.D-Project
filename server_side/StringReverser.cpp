f81e1a88e7776825ac2e4a3a10ef14b44afd0947
evya123 <evya123@gmail.com>
2019-01-03 08:02:40 -0800

    Modified StringReverse::solve

--- server_side/StringReverser.cpp
+++ server_side/StringReverser.cpp
@@ -5,5 +5,5 @@
 #include "StringReverser.h"
 
 std::string StringReverser::solve(std::string problem) {
-
+    std::reverse(problem.begin(), problem.end());
 }
--- server_side/StringReverser.h
+++ server_side/StringReverser.h
@@ -4,10 +4,10 @@
 
 #ifndef S_O_L_I_D_STRINGREVERSER_H
 #define S_O_L_I_D_STRINGREVERSER_H
-
-
 #include "Solver.h"
 #include <string>
+#include <bits/stdc++.h>
+
 class StringReverser : public Solver<std::string,std::string> {
 
     std::string solve(std::string problem) override;
