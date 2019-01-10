3ce15181ef2f0e90c9d8ba2b6486c7dea99fb13c
Lidor Elias <lidor115@gmail.com>
2019-01-10 23:06:44 +0200

    Matrix Searcher

--- Algorithms/MatrixSearcher.cpp
+++ Algorithms/MatrixSearcher.cpp
@@ -14,10 +14,23 @@ State<PAIR > MatrixSearcher::getInitialState() {
 
 std::vector<State<PAIR>> MatrixSearcher::getAllPossibleStates(
         State<PAIR > t) {
-    std::list<State<PAIR>> listOfStates;
+    std::vector<State<PAIR>> listOfStates;
     int i = t.getState().first;
     int j = t.getState().second;
-
+    int size = m_Matrix.size();
+    if (i > 0 && i <= size) {
+        listOfStates.push_back(m_Matrix[i - 1][j]);
+    }
+    if (i >= 0 && i < size) {
+        listOfStates.push_back(m_Matrix[i + 1][j]);
+    }
+    if (j > 0 && j <= size) {
+        listOfStates.push_back(m_Matrix[i][j - 1]);
+    }
+    if (j >= 0 && j < size) {
+        listOfStates.push_back(m_Matrix[i][j + 1]);
+    }
+    return listOfStates;
 }
 
 
